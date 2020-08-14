99d0f33e054184a9c4c9208c8e73532329c366a8
fiorellachilcon <fiorellachilc_20@hotmail.com>
2020-08-14 13:21:50 -0500

    code review

--- __mocks__/node-fetch.mjs
+++ __mocks__/node-fetch.mjs
@@ -1,8 +1,9 @@
-
 const nodeFetch = jest.requireActual('node-fetch');
+// eslint-disable-next-line import/no-extraneous-dependencies
 const fetchMock = require('fetch-mock').sandbox();
+
 Object.assign(fetchMock.config, {
-  fetch: nodeFetch
+  fetch: nodeFetch,
 });
 fetchMock.mock('https://stackoverflow.com/quesl', 404);
 fetchMock.mock('*', 200);
